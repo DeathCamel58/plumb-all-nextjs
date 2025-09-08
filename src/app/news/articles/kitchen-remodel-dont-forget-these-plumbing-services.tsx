6edbd2ee708db3057bcdde5c838b65a82960a866
Dylan Corrales <deathcamel58@gmail.com>
2025-09-08 13:38:39 -0400

    Blog: Update Kitchen Remodel? Don’t Forget These Plumbing Services image

--- src/app/news/articles/kitchen-remodel-dont-forget-these-plumbing-services.tsx
+++ src/app/news/articles/kitchen-remodel-dont-forget-these-plumbing-services.tsx
@@ -8,7 +8,7 @@ export const metadata = {
   title: 'Kitchen Remodel? Don’t Forget These Plumbing Services',
   date: '2025-09-01',
   excerpt: 'Planning a kitchen remodel? Don’t overlook your plumbing! From sink and dishwasher installation to rerouting water and gas lines, these essential services ensure your renovation runs smoothly. Learn what to plan for and why involving a professional plumber early can save time, money, and stress.',
-  image: '/images/running-faucet.jpg',
+  image: '/images/dishwasher-2.jpg',
   path: `/news/article/${slug}`
 };
 
